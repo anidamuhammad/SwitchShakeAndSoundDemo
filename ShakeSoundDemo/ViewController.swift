1a7dd687fcfd8926dd4b7080b0a8eb46b4464d69
user15 <user15@caspian15s-mac-mini.local>
2015-12-18 12:03:09 +0800

    AN

--- ShakeSoundDemo/ViewController.swift
+++ ShakeSoundDemo/ViewController.swift
@@ -7,9 +7,79 @@
 //
 
 import UIKit
+////////////  2 ///////////////////////
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,AVAudioPlayerDelegate {
 
+    
+    ////////////  3    ///////////////
+    var audioPlayer = AVAudioPlayer ()
+    
+    ///////////////// 1 ////////////////
+    //IBOutlet
+    @IBOutlet weak var myLabel: UILabel!
+    
+    //////////////// 5 //////////////
+    //IBAction
+    
+    override func motionBegan(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        myLabel.text  = "Its Shaking !"
+        
+        do{
+            try playSound("shakesound", fileExtension: "wax")
+        } // end of do
+        catch {
+             return
+        } // end of catch
+    } // end of motionBegan
+    
+    
+    ////////////// 4 ///////////////////
+    /////////// func playsound 
+    
+    func playSound(fileName:String,fileExtension:String ) throws {
+        
+        let dispatchQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+        
+        dispatch_async(dispatchQueue,{ let mainBundle = NSBundle.mainBundle()
+            let filePath = mainBundle.pathForResource("\(fileName)", ofType:"\(fileExtension)")
+            
+            if let path = filePath{
+                
+                let fileData = NSData(contentsOfFile: path)
+                
+                do {
+                    //Start the audio player
+                    self.audioPlayer = try AVAudioPlayer(data: fileData!)
+                    
+                    guard let player:  AVAudioPlayer? = self.audioPlayer else {
+                        return
+                    } //end of guard let
+                    
+                    
+                    // Set the delegate and start playing
+                    player!.delegate = self
+                    
+                    if player!.prepareToPlay() && player!.play (){
+                        //Successfully started playing
+                    } else {
+                        //Failed to play
+                    }
+                    
+                } // end of do
+                
+                catch {
+                    return
+                }
+            } // end of path
+            
+        }) //end of let dispatch async
+        
+        
+    } // end of func playSound
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
